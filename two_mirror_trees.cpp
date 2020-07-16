f5f826f632713d8c6c229ec637e701fd6cae0375
rashan <40189953+rashanjotkaur@users.noreply.github.com>
2020-07-15 19:03:08 -0700

    Update two_mirror_trees.cpp

--- two_mirror_trees.cpp
+++ two_mirror_trees.cpp
@@ -1,3 +1,4 @@
+// Method1:
 bool isIdentical(struct Node *r1,struct Node *r2){
     if(r1==NULL && r2==NULL)
         return true;
@@ -18,4 +19,17 @@ void mirror(struct Node **root){
 int areMirror(Node* a, Node* b) {
     mirror(&a);
     return isIdentical(a,b);
+}
+
+// Method 2-Best Approach
+int areMirror(Node* a, Node* b) {
+    if(a==NULL && b==NULL)
+        return 1;
+    if(a==NULL || b==NULL)
+        return 0;
+    if(a->data!=b->data)
+        return 0;
+    int l=areMirror(a->left,b->right);
+    int r=areMirror(a->right,b->left);
+    return l & r;
 }
