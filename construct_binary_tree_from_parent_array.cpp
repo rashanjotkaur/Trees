f1b24a5e28d145afb903e40e1b15f2a1df91a23e
rashan <40189953+rashanjotkaur@users.noreply.github.com>
2020-07-16 23:21:57 -0700

    Update construct_binary_tree_from_parent_array.cpp

--- construct_binary_tree_from_parent_array.cpp
+++ construct_binary_tree_from_parent_array.cpp
@@ -33,3 +33,22 @@ Node *createTree(int parent[], int n){
         insert(&root,i,parent,created);
     return root;
 }
+
+// Method 2
+Node *createTree(int parent[], int n){
+    unordered_map <int,struct Node*> ump;
+    for(int i=0;i<n;i++){
+        struct Node *temp=new Node(i);
+        ump[i]=temp;
+    }
+    int root;
+    for(int i=0;i<n;i++){
+        if(parent[i]==-1)
+            root=i;
+        if(parent[i]!=-1 && ump[parent[i]]->left==NULL)
+            ump[parent[i]]->left=ump[i];
+        else if(parent[i]!=-1 && ump[parent[i]]->right==NULL)
+            ump[parent[i]]->right=ump[i];
+    }
+    return ump[root];
+}
