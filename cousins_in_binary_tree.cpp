f733b58d8cb5176b1242fa5c976990ab5913ec16
rashan <40189953+rashanjotkaur@users.noreply.github.com>
2020-07-29 10:23:18 -0700

    Create cousins_in_binary_tree.cpp

---
+++ cousins_in_binary_tree.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+    typedef pair<struct TreeNode*,struct TreeNode*> pi;
+    bool isCousins(TreeNode* root, int x, int y) {
+        if(root==NULL)
+            return false;
+        pi p1,p2;
+        p1.first=NULL;
+        p1.second=NULL;
+        p2.first=NULL;
+        p2.second=NULL;
+        queue <pi> q;
+        q.push({root,NULL});
+        q.push({NULL,NULL});
+        int level=0;
+        int l1,l2;
+        while(!q.empty()){
+            struct TreeNode *temp=q.front().first;
+            if(temp->val==x){
+                p1=q.front();
+                l1=level;
+            }
+            else if(temp->val==y){
+                p2=q.front();
+                l2=level;
+            }
+            q.pop();
+            if(temp->left)
+                    q.push({temp->left,temp});
+            if(temp->right)
+                    q.push({temp->right,temp});
+            if(q.front().first==NULL && q.front().second==NULL){
+                q.pop();
+                level++;
+                if(!q.empty())
+                    q.push({NULL,NULL});
+            }
+            
+        }
+        if(p1.first==NULL || p2.first==NULL)   
+            return false;
+        if(p1.second==NULL || p2.second==NULL)
+            return false;
+        if(p1.second->val!=p2.second->val && l1==l2)
+            return true;
+        return false;
+    }
